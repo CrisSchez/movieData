1e9d218b490806eb509866f333232864b9167c0f
cris.sanchezrebollo@gmail.com <cris.sanchezrebollo@gmail.com>
2024-06-24 14:52:48 +0000

    newFile
    readingFile

---
+++ movieReading.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+import pandas as pd 
+file_location = "/Workspace/Repos/labuser6933906@vocareum.com/movieData/imdb_top_1000.csv" 
+df = pd.read_csv(file_location)
+
+# COMMAND ----------
+
+
